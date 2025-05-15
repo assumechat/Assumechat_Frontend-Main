5afe95d1494879be26861257bec9b2d88ad5227e
assumechat <assumechatyash@gmail.com>
2025-05-15 20:40:24 +0530

    Revert "Sign up Sign in verified to backend via otp verification"

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,23 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User, UserState } from '@/types/userstate';
-
 const initialState: UserState = {
   user: null,
-  accessToken: null,
-  isAuthenticated: false,
+  isAuthenticated: true,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {setUser: (state,action: PayloadAction<{ user: User; accessToken: string }>) => {
-      state.user = action.payload.user;
-      state.accessToken = action.payload.accessToken;
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
       state.isAuthenticated = true;
     },
     logout: (state) => {
       state.user = null;
-      state.accessToken = null;
       state.isAuthenticated = false;
     },
   },
