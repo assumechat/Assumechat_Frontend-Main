2c1186efb822b9962df58ea76afa886e128eb25a
Khushee21 <khushirathore649@gmail.com>
2025-06-02 19:13:49 +0530

    feedback report done

--- src/app/waitingRoom/tabs/assumer/page.tsx
+++ src/app/waitingRoom/tabs/assumer/page.tsx
@@ -10,6 +10,19 @@ import { getChatSocket } from '@/Services/socketService';
 import { ChatEvent } from '@/types/Chat';
 import { LogOutIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { FaStar } from 'react-icons/fa';
+import { EyeIcon, HeartIcon, ChatIcon } from '@/components/Icons/eye';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from "@/components/ui/dialog";
+
 
 
 const ChatSystem = () => {
@@ -17,6 +30,9 @@ const ChatSystem = () => {
     const messages = useAppSelector(state => state.socket.messages);
     const chatConnected = useAppSelector(state => state.socket.chatConnected);
     const matchedState = useAppSelector(state => state.socket.matched);
+    const [showFeedbackModal ,  setshowFeedbackModal]=useState(false);
+    const [feedbackText , setFeedbackText]= useState('');
+    const [rating , setRating] = useState(3);
     const { position, waiting, online } = useAppSelector(s => s.socket);
     const [inputValue, setInputValue] = useState('');
     const [isMatching, setIsMatching] = useState(!matchedState); // Manual state for UI sync
@@ -24,16 +40,24 @@ const ChatSystem = () => {
     const router = useRouter(); 
     const [peerInfo, setPeerInfo] = useState<{ userId: string; userName?: string } | null>(null);
     // Listen for peer skip/leave events (handled below with getChatSocket)
+    const user = useAppSelector((state) => state.user.user);
+    const [showReportModal , setShowReportModal]= useState(false);
+    const [reportData , setReportData]= useState<any>(null);
+    const [selectedReason, setSelectedReason] = useState('');
+    const [details, setDetails] = useState('');
+    const [showMatchedUser ,  setShowMatchedUser]=useState(false);
 
-//redirect to matched user page 
   useEffect(() => {
     if (matchedState) {
-        setIsMatching(false);
-        router.push('/waitingRoom/tabs/assumer/matchedUser');  
-    } else {
-        setIsMatching(true);
+      setShowMatchedUser(true);
+
+      const timer = setTimeout(() => {
+        setShowMatchedUser(false);      
+      }, 2500);
+
+      return () => clearTimeout(timer);
     }
-}, [matchedState, router]);
+  }, [matchedState]);
 
 
     // Watch for own match updates (Redux)
@@ -58,12 +82,12 @@ const ChatSystem = () => {
             // you can also store these in Redux if you want
             setPeerInfo(payload);
         }
-
         chatSocket.on(ChatEvent.HANDSHAKE, onPeerHandshake);
         return () => {
             chatSocket.off(ChatEvent.HANDSHAKE, onPeerHandshake);
         };
     }, [dispatch]);
+
 
     // Send a chat message
     const handleSend = (e?: React.FormEvent) => {
@@ -78,16 +102,77 @@ const ChatSystem = () => {
     // Skip to next user
     const handleSkip = () => {
         setIsMatching(true);
+         toast.success('Skipped used , Please give feedback');
+        setshowFeedbackModal(true);
         dispatch(clearMessages());
         dispatch(skipMatch());
         //toast.success('Skipped user, searching for a new match...');
-        toast.success('Skipped used , redirecting to feedback page');
-        router.push('/waitingRoom/tabs/assumer/feedback');
-        // handleRetry();
+       // setshowFeedbackModal(true);
+        //  handleRetry();
     };
 
+    //feedback submit handler
+    const handleFeedbackSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!feedbackText.trim() || !rating) return;
+       // console.log("peerRef",peerRef.current?.userId)
+        try {
+            await axios.post(
+                `${process.env.NEXT_PUBLIC_BACKEND_URL}feedback/submit-feedback`,
+                {
+                    feedbackBy: user?._id ,
+                    feedbackTo:peerInfo?.userId,
+                    comment: feedbackText,
+                    rating,
+                },
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            );
+            toast.success('Feedback sent, thank you!');
+            setFeedbackText('');
+            setRating(3);
+            setshowFeedbackModal(false);
+            // Optionally redirect or reset state here
+            setShowReportModal(true);
+        } catch (error) {
+            toast.error('Failed to send feedback. Please try again.');
+            console.error('Feedback submission error:', error);
+        }
+    };
 
-
+    //handle report
+    const reasons = [
+        'Harassment or hate',
+        'Spam / ads',
+        'Inappropriate content',
+        'Impersonation',
+        'Other',
+      ];
+    
+      const handleReportSubmit =async (e: React.FormEvent) => {
+        e.preventDefault();
+        try{
+            await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}feedback/submit-report`, {
+            reasons:selectedReason,
+            details,
+            userId: peerInfo?.userId,
+            name: user?.name
+        },
+        {
+            headers: {'Content-Type' : 'application/json'},
+        }
+    );
+        toast.success("Report submitted successfully");
+        setDetails('');
+        setSelectedReason('');
+        router.push('/waitingRoom/tabs/assumer');
+        }
+        catch(error){
+            console.error("Error submitting report" , error);
+            toast.error("Failed to submitting report");
+        } 
+      };
     const handleRetry = () => {
         // clear any stale chat state
         dispatch(clearMessages());
@@ -154,7 +239,7 @@ const ChatSystem = () => {
                                 />
                             </div>
                             <div>
-                                <h2 className="font-semibold text-[#B30738]">{peerInfo.userName ?? peerInfo.userId}</h2>
+                                <h2 className="font-semibold text-[#B30738]">{peerInfo.userId ?? peerInfo.userName}</h2>
                             </div>
                         </>
                     )}
@@ -238,8 +323,240 @@ const ChatSystem = () => {
                         <FiSend size={20} color='#B30738' />
                     </button>
                 </form>
-            </div>
+               {showFeedbackModal && (
+  <Dialog open={showFeedbackModal} onOpenChange={setshowFeedbackModal}>
+    <DialogContent className="fixed right-0 mt-10 h-[80vh] w-full max-w-4xl bg-white rounded-l-xl p-6 shadow-lg overflow-y-auto">
+      <DialogHeader>
+        <DialogTitle className="text-2xl font-bold text-center mt-8">Chat ended – ready to assume?</DialogTitle>
+        <DialogDescription className="text-center mt-1">
+          Write an assumption about <span className="text-[#B30738] font-semibold">CrypticCloud</span>
+        </DialogDescription>
+      </DialogHeader>
+      <form onSubmit={handleFeedbackSubmit} className="flex flex-col gap-4 mt-1 relative">
+        {/* Feedback input */}
+        <div className="w-full">
+          <label className="text-sm text-gray-500">
+            Based on the chat, guess one hobby or fun fact about them!
+          </label>
+          <input
+            type="text"
+            value={feedbackText}
+            onChange={(e) => setFeedbackText(e.target.value)}
+            className="mt-2 w-full border border-gray-200 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-[#B30738]"
+            placeholder="Type your assumption here…."
+          />
         </div>
+        {/* Star Rating */}
+        <div className="flex items-center gap-2 mt-2">
+          <span className="text-sm text-gray-700">Rate them:</span>
+          {[1, 2, 3].map((star) => (
+            <FaStar
+              key={star}
+              size={20}
+              onClick={() => setRating(star)}
+              className={`${
+                star <= rating ? "text-yellow-400" : "text-gray-300"
+              } cursor-pointer`}
+            />
+          ))}
+        </div>
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="mt-4 bg-[#B30738] text-white px-4 py-2 rounded-lg font-medium hover:bg-[#901e36] transition"
+        >
+          Submit
+        </button>
+      </form>
+    </DialogContent>
+  </Dialog>
+)}
+    {showReportModal && (
+  <Dialog open={showReportModal} onOpenChange={setShowReportModal}>
+    <DialogContent className="fixed right-0 mt-10 h-[80vh] w-full max-w-4xl bg-white rounded-l-xl p-6 shadow-lg overflow-y-auto">
+      
+      <DialogHeader>
+        <DialogTitle className="text-3xl font-bold text-black mb-2 text-center">
+          Report CrypticCloud
+        </DialogTitle>
+        <DialogDescription className="text-gray-600 mb-6 text-center">
+          Tell us what went wrong conversing with <span className="text-[#B30738] font-semibold">CrypticCloud</span>
+        </DialogDescription>
+      </DialogHeader>
+
+      <form onSubmit={handleReportSubmit} className="w-full max-w-lg space-y-4">
+        {reasons.map((reason) => (
+          <label
+            key={reason}
+            className="flex items-center border border-gray-300 rounded px-4 py-2 cursor-pointer hover:border-[#B30738]"
+          >
+            <input
+              type="radio"
+              name="reason"
+              value={reason}
+              checked={selectedReason === reason}
+              onChange={() => setSelectedReason(reason)}
+              className="mr-3 text-[#B30738]"
+            />
+            {reason}
+          </label>
+        ))}
+
+        {selectedReason === 'Other' && (
+          <div className="mt-4">
+            <label className="block text-sm text-gray-600 mb-1">Please Specify:</label>
+            <textarea
+              placeholder="Give us some details about your conversation..."
+              className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-[#B30738]"
+              rows={4}
+              value={details}
+              onChange={(e) => setDetails(e.target.value)}
+            />
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="w-full bg-[#B30738] text-white py-2 px-4 rounded hover:bg-[#B30738] transition"
+        >
+          Submit
+        </button>
+      </form>
+    </DialogContent>
+  </Dialog>
+)}
+
+       { showMatchedUser && (
+        <div className="min-h-screen bg-white flex flex-col items-center justify-center text-center px-4 py-12 relative overflow-hidden">
+              {/* Top-left SVG Background */}
+              <div className="absolute top-0 left-0 -translate-x-1/4 -translate-y-1/4 sm:-translate-x-1/3 sm:-translate-y-1/3">
+                <svg
+                  width="227"
+                  height="283"
+                  viewBox="0 0 227 283"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[150px] h-[187px] sm:w-[227px] sm:h-[283px]"
+                >
+                  <g filter="url(#filter0_f_380_154550)">
+                    <circle cx="85.973" cy="141.973" r="105.473" stroke="#B30738" />
+                    <circle cx="85.4798" cy="141.48" r="78.9798" stroke="#B30738" />
+                    <circle cx="85.9865" cy="141.987" r="52.4865" stroke="#B30738" />
+                    <circle cx="85.5" cy="141.5" r="137" stroke="#B30738" />
+                  </g>
+                </svg>
+              </div>
+        
+              {/* Bottom-right SVG Background */}
+              <div className="absolute bottom-0 right-0 translate-x-1/4 translate-y-1/4 sm:translate-x-1/3 sm:translate-y-1/3">
+                <svg
+                  width="361"
+                  height="396"
+                  viewBox="0 0 361 396"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[240px] h-[263px] sm:w-[361px] sm:h-[396px]"
+                >
+                  <g filter="url(#filter0_f_380_154529)">
+                    <circle cx="197.518" cy="197.518" r="149.018" stroke="#B30738" />
+                    <circle cx="198.139" cy="198.139" r="111.639" stroke="#B30738" />
+                    <circle cx="197.759" cy="197.759" r="74.2591" stroke="#B30738" />
+                    <circle cx="198" cy="198" r="193.5" stroke="#B30738" />
+                  </g>
+                </svg>
+              </div>
+        
+              {/* Heading */}
+              <h1 className="text-4xl font-bold text-black mb-2">You Just Got Matched!</h1>
+              <p className="text-gray-600 mb-8">
+                You're now chatting with{`${peerInfo?.userName}`}
+                <span className="text-rose-700 font-semibold">CrypticCloud</span> – hang out or skip when you're ready.
+              </p>
+        
+              {/* Orbit Avatar Section */}
+             <div className="relative w-[320px] h-[320px] sm:w-[400px] sm:h-[400px]">
+                 {/* Horizontal Ellipse */}
+                    <svg
+                    viewBox="0 0 509 509"
+                    className="absolute w-full h-full animate-spin-slow"
+                    style={{transform: 'rotate(175deg)'}}
+                    xmlns="http://www.w3.org/2000/svg"
+                     >
+                    <ellipse
+                    cx="254.5"
+                    cy="254.5"
+                    rx="253.5"
+                     ry="125"
+                    stroke="#B30738"
+                    strokeWidth="1"
+                    fill="none"
+                    />
+                </svg>
+        
+                 {/* Vertical Ellipse */}
+                    <svg
+                    viewBox="0 0 509 509"
+                    className="absolute w-full h-full animate-spin-slow"
+                    style={{ transform: 'rotate(25deg)' }}
+                     xmlns="http://www.w3.org/2000/svg"
+                    >
+                     <ellipse
+                    cx="254.5"
+                    cy="254.5"
+                    rx="253.5"
+                      ry="125"
+                    stroke="#B30738"
+                    strokeWidth="1"
+                    fill="none"
+                    />
+                    </svg>
+        
+                {/* Avatars */}
+                <div className="absolute top-1/2 left-1/2 -translate-x-[60px] -translate-y-[40px]">
+                  <Image
+                    src="https://res.cloudinary.com/dipywb0lr/image/upload/v1746702005/image_qkwdzs.jpg"
+                    alt="Avatar 1"
+                    width={60}
+                    height={60}
+                    className="rounded-full border-2 shadow-lg"
+                  />
+                </div>
+                <div className="absolute top-1/2 left-1/2 translate-x-[10px] -translate-y-[50px]">
+                  <Image
+                    src="https://res.cloudinary.com/dipywb0lr/image/upload/v1746702005/image_jmhhxy.png"
+                    alt="Avatar 2"
+                    width={60}
+                    height={60}
+                    className="rounded-full border-2 shadow-lg"
+                  />
+                </div>
+        
+                {/* Orbit Icons */}
+                {/* Orbit Icons Positioned Along the Ellipse */}
+        <div className="absolute" style={{ top: '20%', left: '50%', transform: 'translate(-50%, -50%) rotate(-15deg)' }}>
+          <div className="bg-white-blur rounded-full shadow-md p-2 hover:scale-110 transition">
+            <ChatIcon />
+          </div>
+        </div>
+        
+        <div className="absolute" style={{ top: '50%', left: '93%', transform: 'translate(-50%, -50%) rotate(15deg)' }}>
+          <div className="bg-white rounded-full shadow-md p-2 hover:scale-110 transition">
+            <EyeIcon />
+          </div>
+        </div>
+        
+        <div className="absolute" style={{ top: '70%', left: '10%', transform: 'translate(-50%, -50%) rotate(-15deg)' }}>
+          <div className="bg-white rounded-full shadow-md p-2 hover:scale-110 transition">
+            <HeartIcon />
+          </div>
+        </div>
+        
+              </div>
+            </div>
+        )}
+            </div>
+    </div>
+
     );
 };
 
